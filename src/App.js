03a1ba6c3a6263063f6b4f7e96508a100efc4cbd
Chijioke (Chukwu)  Peter <peterchijioke1@gmail.com>
2023-08-31 07:35:58 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
 
+import Chat from "./components/Chat/Chat";
+import Join from "./components/join/Join/Join";
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 const App = () => {
-  return <div>app</div>;
+  return (
+    <Router>
+      <Route path="/" exact component={Join} />
+      <Route path="/chat" component={Chat} />
+    </Router>
+  );
 };
 
 export default App;
